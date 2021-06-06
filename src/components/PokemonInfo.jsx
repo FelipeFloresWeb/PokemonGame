37136aa8ffed65aeb6370089d347d8baa9d0ea40
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-06 18:34:22 -0300

    Correction bug about  informations...

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class PokemonInfo extends Component {
   render() {
     const { info } = this.props;
+    if (!info.name) return <div>Loading informations...</div>
     return (
       <>
       <div>{info.name}</div>
-      <img src={info.sprites.front_default} alt="" />
+      <img src={info.sprites.front_default} alt={info.name} />
       </>
     );
   }
